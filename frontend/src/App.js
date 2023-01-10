6a932986a9a90efbed9df6f85c0a841c0c621718
Richard Kruse <richardkruse3rd@gmail.com>
2023-01-10 20:12:58 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome to CardShop</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
